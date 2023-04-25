52e4aa6dbca59a4d15b60ae97c2b526d83be72dc
shogawa <51148545+shogawa@users.noreply.github.com>
2023-04-25 17:00:47 +0900

    Update arxiv.py

--- arxiv.py
+++ arxiv.py
@@ -12,9 +12,6 @@ QUERY = "(cat:astro-ph.*)+AND+(abs:AGN)+OR+(abs:blackhole)"
 
 
 def parse(data, tag):
-    # parse atom file
-    # e.g. Input :<tag>XYZ </tag> -> Output: XYZ
-
     pattern = "<" + tag + ">([\s\S]*?)<\/" + tag + ">"
     if all:
         obj = re.findall(pattern, data)
@@ -22,7 +19,6 @@ def parse(data, tag):
 
 
 def search(query, start, ids):
-    ids1 = []
     while True:
         counter = 0
 
@@ -32,17 +28,16 @@ def search(query, start, ids):
         url = 'http://export.arxiv.org/api/query?search_query=' + query + '&start=' + str(
             start) + '&max_results=20&sortBy=submittedDate&sortOrder=descending'
 
-        # Get returned value from arXiv API
         req = urllib.request.Request(url)
         with urllib.request.urlopen(req, timeout=10) as res:
             data = res.read().decode()
-        # Parse the returned value
+
         entries = parse(data, "entry")
         for entry in entries:
-            # Parse each entry
+
             url = parse(entry, "id")[0]
             if not (url in ids):
-                # parse
+
                 title = parse(entry, "title")[0]
                 abstract = parse(entry, "summary")[0]
                 date = parse(entry, "published")[0]
@@ -55,27 +50,24 @@ def search(query, start, ids):
                    file.write('URL: ' + url + '  \n')
                    file.write('Published: ' + date + '  \n')
                    file.write(abstract + '  \n\n')
-                ids1.append(url)
+                ids.append(url)
             counter = counter + 1
             if counter == 20:
-                return ids1
+                return ids
         if counter == 0 and len(entries) < 100:
             with open('README.md', 'a') as file:
                 file.write('Currently, there is no available papers' + '\n')
-            return ids1
+            return ids
         elif counter == 0 and len(entries) == 100:
-            # When there is no available paper and full query
             start = start + 100
 
 
 if __name__ == "__main__":
-    # Load log of published data
     if os.path.exists("published.pkl"):
         ids = pickle.load(open("published.pkl", 'rb'))
     else:
         ids = []
 
-    # Query for arXiv API
     query = QUERY
 
     start = 0
@@ -84,8 +76,7 @@ if __name__ == "__main__":
     with open('README.md', 'w') as file:
         file.write(dt + '  \n\n')
 
-    # Call function
     ids = search(query, start, ids)
+    ids = ids[-100:]
 
-    # Update log of published data
     pickle.dump(ids, open("published.pkl", "wb"))
